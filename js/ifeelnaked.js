e7c07b06ad7a8d7ad67597e58b0b3221141111df
Jeff Lyon <jeff@rubbingalcoholic.com>
2015-04-17 15:43:02 -0700

    added modal on click

--- js/ifeelnaked.js
+++ js/ifeelnaked.js
@@ -28,7 +28,7 @@ var addItem = function(item) {
         div.style.backgroundSize = '100% 100%';
 
         div.addEventListener('click', function() {
-            console.log(item)
+            showModal(item);
         }, false);
 
         $('#photos').isotope( 'insert', div );
@@ -40,6 +40,82 @@ var addItems = function(items) {
     for (var i=0; i < items.length; i++) {
         addItem(items[i]);
     }
+}
+
+var showModal = function(item) {
+    var overlay = document.createElement('div');
+    overlay.className = 'overlay invisible';
+
+    var gutter = document.createElement('div');
+    gutter.className = 'gutter';
+
+    var modal = document.createElement('div');
+    modal.className = 'modal photo';
+    
+    var img = document.createElement('div');
+    img.className = 'photo';
+    img.style.background = 'url('+item.photo_url_s3+') center center no-repeat';
+    img.style.backgroundSize = 'auto 100%';
+    modal.appendChild(img);
+
+    var tweet = document.createElement('div');
+    tweet.className = 'tweet';
+
+    var avatar = document.createElement('a');
+    avatar.className = 'avatar';
+    avatar.href = item.original_url;
+    avatar.target = '_blank';
+    var avimg = document.createElement('img');
+    avimg.src = item.user_avatar_s3;
+    avatar.appendChild(avimg);
+    tweet.appendChild(avatar);
+
+    var username = document.createElement('a');
+    username.href = item.original_url;
+    username.target = "_blank";
+    username.textContent = item.username + ":";
+    tweet.appendChild(username);
+
+    if (item.caption.length <= 140)
+        var caption = item.caption;
+    else
+        var caption = item.caption.substr(0, 140) + '…';
+
+    var text = document.createElement('span');
+    text.src = item.user_avatar_s3;
+    text.textContent = ' ' + caption;
+    tweet.appendChild(text);
+
+    var close = document.createElement('a');
+    close.href = '#';
+    close.className = 'close';
+    close.addEventListener('click', function(e) {
+        e.preventDefault();
+        close_modal();
+    }, false);
+    modal.appendChild(close);
+
+
+    modal.appendChild(tweet);
+    
+    gutter.appendChild(modal);
+    overlay.appendChild(gutter);
+    document.body.appendChild(overlay);
+
+    var close_modal = function() {
+        overlay.className = 'overlay invisible';
+        setTimeout(function() {
+            document.body.removeChild(overlay);
+        }, 400);
+    }
+
+    gutter.addEventListener('click', function(e) {
+        if (e.target == gutter)
+            close_modal();
+    }, false);
+
+    overlay.style.display = 'block';
+    setTimeout(function() { overlay.className = 'overlay'; }, 50);
 }
 
 $.ajax('http://ifeelnaked-api.herokuapp.com/random/30', {
